7f2457564e77a489afc941ea0b71ac18aab0a4df
Tomss <tom.cellier@gmail.com>
2026-03-09 16:49:30 +0100

    Configuration dynamique de l'API_URL pour la production

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,7 @@
-export const API_URL = 'http://localhost:3001/api';
+// Si on est en ligne, on utilise l'adresse de Render. Sinon, on utilise localhost.
+export const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+
+export const API_URL = `${BASE_URL}/api`;
+
+// Si jamais tu gères des images stockées sur le serveur
+export const UPLOADS_URL = `${BASE_URL}/uploads`;
