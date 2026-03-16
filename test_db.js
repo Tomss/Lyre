d701c0bab1914b638da8b9c9f5c5948c72fc9679
Tomss <tom.cellier@gmail.com>
2026-03-16 15:08:15 +0100

    chore: cleanup debug diagnostics and revert server logs

--- test_db.js
+++
@@ -1,8 +0,0 @@
-import mysql from 'mysql2/promise';
-async function test() {
-  const db = await mysql.createConnection('mysql://root:ZzOjAZKTGlNfBGxXzvrlEnAezyyMESBf@shortline.proxy.rlwy.net:53715/railway');
-  const [rows] = await db.query('SHOW TABLES;');
-  console.log('Tables in Railway DB:', rows);
-  process.exit(0);
-}
-test();
